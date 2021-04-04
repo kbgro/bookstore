b1b796cde26059998226c19841daeb4594dbb221
kbgro <daniendegwa@gmail.com>
2021-04-04 17:19:21 +0300

    auth app urls

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("pages.urls"))]
+urlpatterns = [
+    # Django admin
+    path("admin/", admin.site.urls),
+    # User management
+    path("accounts/", include("django.contrib.auth.urls")),  # new
+    # Local apps
+    path("", include("pages.urls")),
+]
