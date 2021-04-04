8b86edbd18e3057060d68d7f7e1b3e3f907d4a6c
kbgro <daniendegwa@gmail.com>
2021-04-04 16:30:32 +0300

    setup method

--- pages/tests.py
+++ pages/tests.py
@@ -5,27 +5,22 @@ from django.urls.base import reverse
 class HomepageTests(SimpleTestCase):
     """Home page view tests."""
 
+    def setUp(self) -> None:
+        url = reverse("home")
+        self.response = self.client.get(url)
+
     def test_home_page_status_code(self):
         """Testing status code."""
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
-
-    def test_home_page_url_name(self):
-        """Testing url name"""
-        response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_hompage_template(self):
         """Testing Templates"""
-        response = self.client.get(reverse("home"))
-        self.assertTemplateUsed(response, "home.html")
+        self.assertTemplateUsed(self.response, "home.html")
 
     def test_hompage_contains_correct_html(self):
         """Testing HTML"""
-        response = self.client.get(reverse("home"))
-        self.assertContains(response, "Homepage")
+        self.assertContains(self.response, "Homepage")
 
     def test_hompage_does_not_contains_incorrect_html(self):
         """Testing HTML not containing incorrect html"""
-        response = self.client.get(reverse("home"))
-        self.assertNotContains(response, "Hi there! I should not be on the page.")
+        self.assertNotContains(self.response, "Hi there! I should not be on the page.")
