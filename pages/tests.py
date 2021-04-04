b10ad3dbe6cef9a74e738538cd85fa4bbe5825b1
kbgro <daniendegwa@gmail.com>
2021-04-04 16:35:46 +0300

    tets resolve

--- pages/tests.py
+++ pages/tests.py
@@ -1,5 +1,7 @@
 from django.test.testcases import SimpleTestCase
-from django.urls.base import reverse
+from django.urls.base import resolve, reverse
+
+from .views import HomePageView
 
 
 class HomepageTests(SimpleTestCase):
@@ -24,3 +26,7 @@ class HomepageTests(SimpleTestCase):
     def test_hompage_does_not_contains_incorrect_html(self):
         """Testing HTML not containing incorrect html"""
         self.assertNotContains(self.response, "Hi there! I should not be on the page.")
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
