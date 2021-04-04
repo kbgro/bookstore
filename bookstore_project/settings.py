06ae6cd94fcb6b74bf9bc80ed053089d85b82bee
kbgro <daniendegwa@gmail.com>
2021-04-04 13:59:28 +0300

    resgister users

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # local
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
@@ -110,3 +112,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+AUTH_USER_MODEL = "users.CustomUser"
