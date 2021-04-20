d0af4b8f5c895ac68db49fea963cc315da1cff49
kbgro <daniendegwa@gmail.com>
2021-04-20 21:22:17 +0300

    Review in Book Admin

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Review
+
+
+class ReviewInline(admin.TabularInline):
+    model = Review
 
 
 @admin.register(Book)
@@ -18,3 +22,4 @@ class BookAdmin(admin.ModelAdmin):
         "author",
     )
     ordering = ("author",)
+    inlines = [ReviewInline]
