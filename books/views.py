5199e56971b065e55bf9b2c0c8d3cfe0581fc98d
kbgro <daniendegwa@gmail.com>
2021-07-25 16:33:09 +0300

    add review support for each book

--- books/views.py
+++ books/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.db.models import Q
+from django.shortcuts import redirect, get_object_or_404
 from django.views.generic import DetailView, ListView
 
-from .models import Book
+from .models import Book, Review
 
 
 class BookListView(LoginRequiredMixin, ListView):
@@ -18,6 +19,22 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     context_object_name = "book"
     login_url = "account_login"
     permission_required = "books.special_status"
+
+    def post(self, request, *args, **kwargs):
+        next_ = request.POST.get('next', '/')
+        book_id = next_.split("/")[-1]
+        user_review = request.POST.get('review', "")
+        if not user_review:
+            return redirect(next_)
+        # book
+        book = get_object_or_404(Book, id=book_id)
+        # create review
+        Review.objects.create(
+            book=book,
+            author=request.user,
+            review=user_review,
+        )
+        return redirect(next_)
 
 
 class SearchListView(ListView):
