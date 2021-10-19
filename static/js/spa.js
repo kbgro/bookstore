8d067492276d86783313acab9dbd3481d5806dc0
kbgro <daniendegwa@gmail.com>
2021-10-19 13:12:00 +0300

    implement javascript cart

---
+++ static/js/spa.js
@@ -0,0 +1,47 @@
+class Cart {
+  #orders = []
+  #amount = 0
+
+  constructor() {
+    if (!Cart.instance) {
+      Cart.instance = this;
+      // this.orders = [];
+    }
+    // Initialize object
+    return Cart.instance
+  }
+
+  // Properties & Methods
+  add(product) {
+    if (this.#orders.filter(e => e.id === product.id).length > 0)
+      return false;
+    this.#orders.push(product)
+    this.#amount += product.price;
+    this.#amount = this.roundAmount(this.#amount);
+    $("#cart-items").html(this.#orders.length)
+    return true;
+  }
+
+  remove(product) {
+    this.#orders = this.#orders.filter(((value) => value.id !== product.id))
+    this.#amount -= product.price;
+    this.#amount = this.roundAmount(this.#amount);
+  }
+
+  roundAmount(amount) {
+    return Math.round((amount + Number.EPSILON) * 100) / 100;
+  }
+
+  total() {
+    return this.#amount;
+  }
+
+  clear() {
+    this.#orders = [];
+  }
+}
+
+const instance = new Cart()
+Object.freeze(instance)
+
+export default instance
