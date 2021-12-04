893263b416838e23495b3ae2f6b104c61809709d
kbgro <daniendegwa@gmail.com>
2021-12-05 02:27:52 +0300

    post order

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,12 @@
+import json
+
 import stripe
 from django.conf import settings
 from django.contrib.auth.models import Permission
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+
+from books.models import Book
 
 
 class OrdersPageView(TemplateView):
@@ -12,6 +16,15 @@ class OrdersPageView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["stripe_key"] = settings.STRIPE_TEST_PUBLISHABLE_KEY
         return context
+
+    def post(self, request, *args, **kwargs):
+        post_dict = request.POST.dict()
+        items = json.loads(post_dict.get("items"))
+        total = float(post_dict.get("total"))
+        books = Book.objects.filter(id__in=items)
+        print((items, total))
+        print(books)
+        return redirect("orders")
 
 
 def charge(request):
